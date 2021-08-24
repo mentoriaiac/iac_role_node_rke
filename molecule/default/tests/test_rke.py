8aaa6a1fad8b8221a88f011e94194b20ca8b2a41
lipenodias <lipenodias@gmail.com>
2021-08-24 13:57:01 -0300

    adicionado, tasks, teste e ci

--- molecule/default/tests/test_rke.py
+++ molecule/default/tests/test_rke.py
@@ -20,19 +20,15 @@ def test_hosts_file(host):
         f = host.file(name).exists
 
 def test_docker_kubectl_is_installed(host):
-    index = 0
-    version = ('5:20.10','5:20.10','1.4.9','1.22')
-    for name in (
-     "docker-ce",
-     "docker-ce-cli",
-     "containerd.io",
-     "kubectl",
-    ):
+    
+    version = ['5:20.10','5:20.10','1.4.9','1.22']
+    packages = ['docker-ce','docker-ce-cli','containerd.io','kubectl']
+
+    for i, name in enumerate(packages):
 
         docker = host.package(name)
         assert docker.is_installed
-        assert docker.version.startswith(version[index])
-        index+=1
+        assert docker.version.startswith(version[i])
 
 # Verificando se docker está rodando
 
@@ -57,16 +53,13 @@ def test_if_container_is_removed(host):
 # Verificando conteudo de arquivos
 
 def test_if_ssh_config_is_ok(host):
-    index = 0
-    conteudo = ('AllowTcpForwarding yes','net.bridge.bridge-nf-call-iptables')
-    for name in (
-     "/etc/ssh/ssh_config",
-     "/etc/sysctl.d/k8s.conf",
-    ):
-        file = host.file(name)
 
-        assert file.contains(conteudo[index])
-        index+=1
+    conteudo = ['AllowTcpForwarding yes','net.bridge.bridge-nf-call-iptables']
+    path = ['/etc/ssh/ssh_config','/etc/sysctl.d/k8s.conf']
+    for i, name in enumerate(path):
+
+        file = host.file(name)
+        assert file.contains(conteudo[i])
 
 # Verificando usuário k8s
 
@@ -76,4 +69,3 @@ def test_if_user_k8s_exists(host):
 
     assert rke_user.exists
     assert rke_user.groups == ['k8s', 'docker']
-
