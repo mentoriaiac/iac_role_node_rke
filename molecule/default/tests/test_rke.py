1920e8e2479db639424c82c25ef65e3dd84f468d
lipenodias <lipenodias@gmail.com>
2021-09-12 17:41:03 -0300

    fix: Corrigido o problema com modprobe
        Durante a execução do packer foi identificado que o systctl não
        estava funcionando.
    
        Para a correção do issue, na role do ansible foi incluido a task
        de habilitar o modulo br_netfiler utilizando modprobe.
    
        O molecule necessitou algumas configurações extras, incluindo um
        bind de volume /lib/modules no container, para permitir o module
        se carregado. Feito ajuste também no test infra para buscar os
        novos parametros do bridge.
    
        Revisado por:
        - Sebawebber
        - Danilo Rocha
        - Luiz Aoqui
        - ramonboorges
        - Brunareisalm
        - Juan maia
        - robertoromeu
        Refs #4

--- molecule/default/tests/test_rke.py
+++ molecule/default/tests/test_rke.py
@@ -13,7 +13,7 @@ def test_hosts_file(host):
     for name in (
      "/usr/bin/kubectl",
      "/etc/docker/daemon.json",
-     "/etc/sysctl.d/k8s.conf",
+     "/etc/sysctl.d/20-net-bridge.conf",
     ):
 
         f = host.file(name)
@@ -62,8 +62,8 @@ def test_if_container_is_removed(host):
 
 def test_if_ssh__and_k8s_config_is_ok(host):
 
-    conteudo = ['AllowTcpForwarding yes','net.bridge.bridge-nf-call-iptables']
-    path = ['/etc/ssh/ssh_config','/etc/sysctl.d/k8s.conf']
+    conteudo = ['AllowTcpForwarding yes','net.bridge.bridge-nf-call-iptables', 'net.bridge.bridge-nf-call-ip6tables']
+    path = ['/etc/ssh/ssh_config','/etc/sysctl.d/20-net-bridge.conf', '/etc/sysctl.d/20-net-bridge.conf']
 
     for i, name in enumerate(path):
 
